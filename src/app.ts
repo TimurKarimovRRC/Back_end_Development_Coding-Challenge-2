0572624ee6ae005a3c3a6bdb32e4761a2ae58e91
Timur Karimov <tkarimov@academic.rrc.ca>
2026-02-03 17:36:45 -0600

    connect healthRoutes

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,10 @@
 import express from "express";
+import { healthRoutes } from "./api/v1/routes/healthRoutes";
+import { eventRoutes } from "./api/v1/routes/eventRoutes";
+
 export const app = express();
+
+app.use(express.json());
+
+app.use("/api/v1", healthRoutes);
+app.use("/api/v1", eventRoutes);
